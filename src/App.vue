88eb08e2a984dda96921eef7b56be1be50e42766
DomenicoLerose85 <domenicolerose85@libero.it>
2022-05-06 17:37:48 +0200

    maincomponent_created

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
     <header-component />
+    <main-component />
   </div>
 </template>
 
 
 <script>
 import HeaderComponent from "./components/HeaderComponent.vue";
+import MainComponent from "./components/MainComponent.vue";
 
 export default {
   name: "App",
   components: {
     HeaderComponent,
+    MainComponent,
   },
 };
 </script>
---
+++ src/components/MainComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <main></main>
+</template>
+
+<script>
+export default {
+  name: "MainComponent",
+  props: [],
+};
+</script>
+
+<style  lang="scss">
+@import "@/assets/styles/vars.scss";
+main {
+  width: 100%;
+  height: 800px;
+  background-color: $bg-color;
+}
+</style>
