04c34fd6ce150ffd99561f1e37af65063350438f
DomenicoLerose85 <domenicolerose85@libero.it>
2022-05-07 19:18:34 +0200

    api_call_done

--- src/components/MainGrid.vue
+++ src/components/MainGrid.vue
@@ -1,11 +1,30 @@
 <template>
-  <main></main>
+  <div>
+    <div v-for="(item, index) in discList" :key="item.id">
+      id {{ item.id }} poster{{ item.poster }} title{{ item.title }} author{{
+        item.author
+      }}
+    </div>
+  </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "MainGrid",
-  props: [],
+  data() {
+    return {
+      discList: [],
+      endPoint: "https://flynn.boolean.careers/exercises/api/array/",
+    };
+  },
+  mounted() {
+    axios.get(this.endPoint + "music").then((res) => {
+      console.log(res);
+      this.discList = res.data;
+    });
+  },
 };
 </script>
 
